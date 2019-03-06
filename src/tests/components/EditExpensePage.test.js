99a73431026d706bec01ce193dfff75922d8d87c
Bhargavi Chennupati <bcgmm@mail.umkc.edu>
2019-03-05 21:33:23 -0600

    firbase

--- src/tests/components/EditExpensePage.test.js
+++ src/tests/components/EditExpensePage.test.js
@@ -3,6 +3,7 @@ import { shallow } from 'enzyme';
 import expenses from '../fixtures/expenses';
 import { EditExpensePage } from '../../components/EditExpensePage';
 
+
 let editExpense, removeExpense, history, wrapper;
 
 beforeEach(() => {
