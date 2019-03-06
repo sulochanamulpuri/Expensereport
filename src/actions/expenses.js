9c63a5bed7910ad0be472e17408689e43465cc56
Bhargavi Chennupati <bcgmm@mail.umkc.edu>
2019-03-06 01:53:57 -0600

    last

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -17,7 +17,7 @@ export const startAddExpense = (expenseData = {}) => {
     } = expenseData;
     const expense = { description, note, amount, createdAt };
 
-    database.ref('expenses').push(expense).then((ref) => {
+    return database.ref('expenses').push(expense).then((ref) => {
       dispatch(addExpense({
         id: ref.key,
         ...expense
@@ -32,9 +32,48 @@ export const removeExpense = ({ id } = {}) => ({
   id
 });
 
+export const startRemoveExpense = ({ id } = {}) => {
+  return (dispatch) => {
+    return database.ref(`expenses/${id}`).remove().then(() => {
+      dispatch(removeExpense({ id }));
+    });
+  };
+};
+
 // EDIT_EXPENSE
 export const editExpense = (id, updates) => ({
   type: 'EDIT_EXPENSE',
   id,
   updates
 });
+
+export const startEditExpense = (id, updates) => {
+  return (dispatch) => {
+    return database.ref(`expenses/${id}`).update(updates).then(() => {
+      dispatch(editExpense(id, updates));
+    });
+  };
+};
+
+// SET_EXPENSES
+export const setExpenses = (expenses) => ({
+  type: 'SET_EXPENSES',
+  expenses
+});
+
+export const startSetExpenses = () => {
+  return (dispatch) => {
+    return database.ref('expenses').once('value').then((snapshot) => {
+      const expenses = [];
+
+      snapshot.forEach((childSnapshot) => {
+        expenses.push({
+          id: childSnapshot.key,
+          ...childSnapshot.val()
+        });
+      });
+
+      dispatch(setExpenses(expenses));
+    });
+  };
+};
--- src/playground/promises.js
+++ src/playground/promises.js
@@ -1,19 +1,27 @@
 const promise = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    // resolve({
-    //   name: 'sulochana,
-    //   age: 26
-    // });
-    reject('Something went wrong!');
-  }, 5000);
+setTimeout(() => {
+  resolve({
+    name: 'sulochana',
+    age: 26
+  });
+  // reject('Something went wrong!');
+}, 5000);
 });
 
 console.log('before');
 
 promise.then((data) => {
-  console.log('1', data);
+console.log('1', data);
+
+return new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve('This is my other promise');
+  }, 5000);
+});
+}).then((str) => {
+console.log('does this run?', str);
 }).catch((error) => {
-  console.log('error: ', error);
+console.log('error: ', error);
 });
 
 console.log('after');
